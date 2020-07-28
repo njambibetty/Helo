7234ac22b03729c9712be3f35324f7e2db7e750c
njambibetty <njmabiwan@gmail.com>
2020-07-28 12:10:22 +0300

    This are widgets and some icons

--- app/src/main/java/com/example/helo/RegistrationActivity.kt
+++ app/src/main/java/com/example/helo/RegistrationActivity.kt
@@ -2,10 +2,26 @@ package com.example.helo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_registration.*
 
 class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+        btnbutton.setOnClickListener{
+            var firstName = etFirstName.text.toString()
+            var lastName= etlastname.text.toString()
+            var email= etEmail.text.toString()
+            var phone= etPhone.text.toString()
+            var password= etConfirm.text.toString()
+            //Toast.makeText(baseContext, lastName, Toast.LENGTH_SHORT.show()
+            //Toast.makeText(baseContext, email, Toast.LENGTH_SHORT.show)()
+            //Toast.makeText(baseContext, phone, Toast.LENGTH_SHORT.show)()
+            //Toast.makeText(baseContext, password, Toast.LENGTH_SHORT.show)()
+
+        }
+        }
     }
-}
+
